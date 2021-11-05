afd3df0b67f63b52d1b884aa88edfe7e8df251e6
cexll <evanxian9@gmail.com>
2021-11-05 17:18:47 +0800

    add templates

--- cgin/cgin.go
+++ cgin/cgin.go
@@ -1,8 +1,10 @@
 package cgin
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path"
 	"strings"
 )
 
@@ -18,8 +20,10 @@ type RouterGroup struct {
 
 type Engine struct {
 	*RouterGroup
-	router *router
-	groups []*RouterGroup
+	router        *router
+	groups        []*RouterGroup
+	htmlTemplates *template.Template
+	funcMap       template.FuncMap
 }
 
 func New() *Engine {
@@ -27,6 +31,33 @@ func New() *Engine {
 	engine.RouterGroup = &RouterGroup{engine: engine}
 	engine.groups = []*RouterGroup{engine.RouterGroup}
 	return engine
+}
+
+func (engine *Engine) SetFuncMap(funcMap template.FuncMap) {
+	engine.funcMap = funcMap
+}
+
+func (engine *Engine) LoadHTMLGlob(pattern string) {
+	engine.htmlTemplates = template.Must(template.New("").Funcs(engine.funcMap).ParseGlob(pattern))
+}
+
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs))
+	return func(c *Context) {
+		file := c.Param("filepath")
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+		fileServer.ServeHTTP(c.Writer, c.Req)
+	}
+}
+
+func (group *RouterGroup) Static(relativePath string, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	group.GET(urlPattern, handler)
 }
 
 func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
@@ -61,7 +92,7 @@ func (group *RouterGroup) Group(prefix string) *RouterGroup {
 	return newGroup
 }
 
-func (group *RouterGroup) Use(middlewares ...HandlerFunc)  {
+func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
 	group.middlewares = append(group.middlewares, middlewares...)
 }
 
@@ -75,5 +106,6 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 	c := newContext(w, req)
 	c.handlers = middlewares
+	c.engine = engine
 	engine.router.handle(c)
 }
--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"time"
 
 	"cgin"
 )
+
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
 
 func onlyForV2() cgin.HandlerFunc {
 	return func(c *cgin.Context) {
@@ -18,35 +30,63 @@ func onlyForV2() cgin.HandlerFunc {
 
 func main() {
 	r := cgin.New()
+	r.Use(cgin.Logger())
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
 
+	stu1 := &student{Name: "Cexll", Age: 20}
+	stu2 := &student{Name: "TheShy", Age: 22}
 	r.GET("/", func(c *cgin.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello cGin</h1>")
+		c.HTML(http.StatusOK, "css.tmpl", nil)
 	})
 
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/", func(c *cgin.Context) {
-			c.HTML(http.StatusOK, "<h1>Hello cGin </h1>")
+	r.GET("/students", func(c *cgin.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", cgin.H{
+			"title":  "cgin",
+			"stuArr": [2]*student{stu1, stu2},
 		})
+	})
 
-		v1.GET("/hello", func(c *cgin.Context) {
-			c.String(http.StatusOK, "Hello %s, you're at %s\n", c.Query("name"), c.Path)
+	r.GET("/date", func(c *cgin.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", cgin.H{
+			"title": "cgin",
+			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
 		})
-	}
-	v2 := r.Group("/v2")
-	v2.Use(onlyForV2())
-	{
-		v2.GET("/hello/:name", func(c *cgin.Context) {
-			c.String(http.StatusOK, "Hello %s, you're at %s\n", c.Param("name"), c.Path)
-		})
+	})
+	r.Run(":9999")
 
-		//v2.POST("/login", func(c *cgin.Context) {
-		//	c.JSON(http.StatusOK, cgin.H{
-		//		"username": c.PostForm("username"),
-		//		"password": c.PostForm("password"),
-		//	})
-		//})
-	}
+	//r.GET("/", func(c *cgin.Context) {
+	//	c.HTML(http.StatusOK, "<h1>Hello cGin</h1>")
+	//})
+	//
+	//v1 := r.Group("/v1")
+	//{
+	//	v1.GET("/", func(c *cgin.Context) {
+	//		c.HTML(http.StatusOK, "<h1>Hello cGin </h1>")
+	//	})
+	//
+	//	v1.GET("/hello", func(c *cgin.Context) {
+	//		c.String(http.StatusOK, "Hello %s, you're at %s\n", c.Query("name"), c.Path)
+	//	})
+	//}
+	//v2 := r.Group("/v2")
+	//v2.Use(onlyForV2())
+	//{
+	//	v2.GET("/hello/:name", func(c *cgin.Context) {
+	//		c.String(http.StatusOK, "Hello %s, you're at %s\n", c.Param("name"), c.Path)
+	//	})
+	//	r.Static("/assets", "./static")
+
+	//v2.POST("/login", func(c *cgin.Context) {
+	//	c.JSON(http.StatusOK, cgin.H{
+	//		"username": c.PostForm("username"),
+	//		"password": c.PostForm("password"),
+	//	})
+	//})
+	//}
 	//r.GET("/hello", func(c *cgin.Context) {
 	//	//
 	//	c.String(http.StatusOK, "Hello %s, you`re at %s\n", c.Query("name"), c.Path)
@@ -67,5 +107,4 @@ func main() {
 	//	})
 	//})
 
-	r.Run(":9999")
 }
