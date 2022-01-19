05a6d49aa06ad30b4871b45f2cef2fef1c253934
cexll <evanxian9@gmail.com>
2022-01-19 13:49:19 +0800

    add

--- lin_test.go
+++ lin_test.go
@@ -1,1 +1,21 @@
 package lin
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestStart(t *testing.T) {
+	r := New()
+	r.GET("/", func(c *Context) {
+		c.String(http.StatusOK, "Hello Lin!")
+	})
+	r.GET("/hello", func(c *Context) {
+		c.String(http.StatusOK, "Hello %s, you' re at %s\n!", c.Query("name"), c.Path)	
+	})
+	v1 := r.Group("/v1")
+	v1.GET("/", func (c *Context) {
+		c.String(http.StatusOK, "hello")
+	})
+	r.Run(":9090")
+}
